9028634a958fb62255c082b64d64fb57d1669c03
Vitor <vitorcarvalho.dev@gmail.com>
2022-09-10 22:08:38 -0300

    praticando java

---
+++ Lógica Programação/javaBasicEx/src/NumeroPrimo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NumeroPrimo {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+
+        int start, end, primo= 0; 
+
+        System.out.println("Digite o início da contagem: ");
+        start = ent.nextInt();
+        System.out.println("Digite o final da contagem");
+        end = ent.nextInt();
+
+
+    }
+}
