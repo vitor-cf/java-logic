230a955e8ce2f2346b70d51cecdfb765988f6abc
Vitor <vitorcarvalho.dev@gmail.com>
2022-10-03 22:04:08 -0300

    JAVA QUEBEC DIO

--- Lógica Programação/java-logic-exercise/src/Beecrowd/A1064.java
+++ Lógica Programação/java-logic-exercise/src/Beecrowd/A1064.java
@@ -3,26 +3,56 @@
  import java.util.*;
  public class A1064 {
      public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner (System.in);
+		double x,y,conta;
+		String operator;
+		String Saida;
+        
+		System.out.println("Informe os dois valores desejados");
+		x= sc.nextDouble();
+		y= sc.nextDouble();
 
-         int contador = 1 , qtd = 0;
-         double somaValores = 0;
- 
-         while (contador <= 6) {
-            double valor = sc.nextDouble();
-            
-             if (valor > 0) {
-                 qtd++;
-                 somaValores += valor;
-                 
-                }
-                contador++;  
-                
-                
-               }
-               System.out.println(qtd + " valores positivos" + 
-               "\n" + (somaValores / qtd));
-            
+		System.out.println("Escolha a operação que deseja realizar:\n" + "a)Soma\n"+ "b)Subtração\n" + "c)Multiplicação\n"+ "d)Divisão");
+		
+		operator=sc.next();
+		
+		while(operator.equals("a") == false &&  operator.equals("b") == false && operator.equals("c") == false  && operator.equals("d") == false) {
+		
+			System.out.println("Escolha a operação que deseja realizar:\n" + "a)Soma\n"+ "b)Subtração\n" + "c)Multiplicação\n"+ "d)Divisão");
+			
+			operator=sc.next();
+			}
+			
+			switch (operator) {
+		case "a":
+			System.out.println("Operação solicitada Soma\n");
+			 conta= x+y;
+			System.out.println("Resultado da sua operação foi " + conta);
+			break;
+		case "b":
+			System.out.println("Operação selecionada Subtração\n");
+			conta= x-y;
+			System.out.println("Resultado da sua operação foi " + conta);
+			break;
+
+		case "c":
+			System.out.println("Operação selecionada foi Multiplicação\n");
+			conta= x*y;
+			System.out.println("Resultado da sua operação foi "+ conta);
+	break;
+
+		case "d":
+			System.out.println("Operação selecionada foi a Divisão\n");
+			conta= x/y;
+			System.out.println("Resultado da sua operação foi "+ conta);
+	
+	break;
+		
+
+		default: System.out.println("Alternativa inserida não existe/n");
+		
+			break;
+		}
         }
             
     }
--- javaQuebec/QuebecDio/src/quebec/dio/Main.java
+++ javaQuebec/QuebecDio/src/quebec/dio/Main.java
@@ -7,12 +7,29 @@ public class Main {
 		Calculadora.soma(3, 6);
 		Calculadora.subtracao(9, 1.8);
 		Calculadora.multiplicacao(7, 8);
-		Calculadora.divisao(5, 2.5);
+		Calculadora.divisao(5, 2.5); 
+		
+		
 		
 		// Mensagem
 		System.out.println("Exercício mensagem");
 		Mensagem.obterMensagem(9);
 		Mensagem.obterMensagem(14);
-		Mensagem.obterMensagem(1);
+		Mensagem.obterMensagem(1); 
+		
+		// Quadrilatero 
+		System.out.println("Exercício quadrilatero");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5d,5d );
+		Quadrilatero.area(7, 8, 9);
+		Quadrilatero.area(5f, 5f); 
+		
+		// Quadrilatero usando return
+		double areaQuadrado = Quadrilatero.area(3);
+		System.out.println(areaQuadrado);
+		
+		double areaRetangulo = Quadrilatero.area(5,5);
+		System.out.println(areaRetangulo);
+		
 	}
 }
